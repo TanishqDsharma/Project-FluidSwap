a722d9468abdf5816e17c0f39fdd4e22343d60af
TanishqDsharma <tanishqsharma612@gmail.com>
2024-09-15 12:02:30 +0000

    Added more function related to Pricing

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -18,8 +18,37 @@ function addLiquidity(uint256 _tokenAmount) public payable{
 } 
 
 
+function getPrice(
+    uint256 inputReserve, 
+    uint256 outputReserve) public pure returns(uint256){
+
+        require(inputReserve>0&&outputReserve>0,"Not enough in the Reserves");
+        return (inputReserve*1000)/outputReserve;
+}
+
+
+function getAmount(
+    uint256 inputAmount,
+    uint256 inputReserve,
+    uint256 outputReserve
+) private pure returns(uint256){
+
+    require(inputReserve>0&&outputReserve>0,"Not enough in the Reserves");
+    return (inputAmount*inputReserve)/(inputReserve+inputAmount);
+
+}
+
+function getTokenAmount(
+    uint256 _ethSold
+) public view returns(uint256) {
+    require(_ethSold>0,"ethSold is very small");
+    uint256 tokenReserve = getReserve();
+    return getAmount(_ethSold,tokenReserve,address(this).balance);
+}
+
 function getReserve() public view returns(uint256){
     return IERC20(tokenAddress).balanceOf(address(this));
 }
+
 
 }
