52a24e2222dd978255d1135f4abcd35fd69b8da4
TanishqDsharma <tanishqsharma612@gmail.com>
2024-09-16 09:57:47 +0000

    Add Liqudity Function Implemented

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -1,20 +1,47 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 import {IERC20} from "../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-contract Exchange{
+
+
+contract Exchange is ERC20{
 
 address public tokenAddress;
 
-constructor(address _token){
+constructor(address _token) ERC20("Zswap-V1", "ZSP"){
     require(_token!=address(0),"Invalid Token Address");
     tokenAddress=_token;
 }
 
-function addLiquidity(uint256 _tokenAmount) public payable{
-    IERC20 token = IERC20(tokenAddress);
-    token.transferFrom(msg.sender,address(this),_tokenAmount);
+function addLiquidity(uint256 _tokenAmount) public payable returns(uint256){
+    if(getReserve()==0){
+        IERC20 token = IERC20(tokenAddress);
+        token.transferFrom(msg.sender,address(this),_tokenAmount);
+        // Since, the pool is being intialized the amount of liqudity
+        // is equal to the amount of ETH in the pool. 
+        // Example: If the user sent 10 ETH, liquidity = 10.
+        uint256 liquidity = address(this).balance;
+        _mint(msg.sender, liquidity);
+        return liquidity;
+
+    }else{
+        uint256 ethReserve = address(this).balance - msg.value;
+        uint256 tokenReserve = getReserve();
+        // Here, tokenAmount is the amount of tokens that the user needs to provide to 
+        // maintain the same ETH-to-token ratio in the pool.
+        uint256 tokenAmount = msg.value*tokenReserve/ethReserve;
+        require(_tokenAmount >= tokenAmount, "insufficient token amount");
+        IERC20 token = IERC20(tokenAddress);
+        token.transferFrom(msg.sender, address(this), tokenAmount);  
+        //The LP tokens (liquidity) issued are proportional to the ETH the user is adding, relative to the total ETH reserve.
+        
+        uint256 liquidity = (totalSupply() * msg.value) / ethReserve;
+        _mint(msg.sender, liquidity);
+        return liquidity;
+      
+    }
 } 
 
 
