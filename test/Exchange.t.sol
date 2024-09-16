2d88e745e92d34b4c4ebb9d804f68d9db00288f4
TanishqDsharma <tanishqsharma612@gmail.com>
2024-09-16 09:58:19 +0000

    Incomplete Test Suite

--- test/Exchange.t.sol
+++ test/Exchange.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 import {Test,console} from "../lib/forge-std/src/Test.sol";
 import {Exchange} from "../src/Exchange.sol";
